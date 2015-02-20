544923c52850277682fc9bd5644040a4326f643b
Adam Walter <adam@adamwalter.com>
2015-02-20 10:14:41 -0500

    Revise and reorganize

--- wp-rewrite-api-boilerplate.php
+++ wp-rewrite-api-boilerplate.php
@@ -1,52 +1,40 @@
 <?php
 
 /**
- * Plugin Name: Custom URL Rewrites
- * Plugin URI:  https://github.com/vitaldevteam/
- * Description: Creates custom URL rewrites using the WordPress Rewrite API
- * Version:     1.0.0
- * Author:      Vital
- * Author URI:  http://vtldesign.com/
- * License:     GPL2
+ * Plugin Name:       Custom URL Rewrites
+ * Version:           1.0.0
+ * Author:            Vital
+ * Author URI:        http://vital.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-if ( ! defined( 'WPINC' ) ) {
-    die;
-}
+if ( !defined('WPINC') ) die;
 
-/*   Plugin activation
-    --------------------------------------------------------------------------  */
 
 function vtl_url_rewrite_activate() {
-
     vtl_url_rewrite_rules();
     flush_rewrite_rules();
-
 }
-
-
-/*   Plugin deactivation
-    --------------------------------------------------------------------------  */
 
 function vtl_url_rewrite_deactivate() {
-
     flush_rewrite_rules();
-
 }
-
-
-/*   Register rewrites
-    --------------------------------------------------------------------------  */
 
 function vtl_url_rewrite_rules() {
 
-    add_rewrite_rule('^nutrition/([^/]*)/([^/]*)/?', 'index.php?page_id=42&food=$matches[1]&variety=$matches[2]', 'top');
+    add_rewrite_rule('widgets/([^/]*)', 'index.php?page_id=1892&widget_type=$matches[1]', 'top');
 
-    add_rewrite_tag('%food%', '([^&]+)');
-    add_rewrite_tag('%variety%', '([^&]+)');
+    add_rewrite_tag('%widget_type%', '([^&]+)');
+
 }
 
 
+// Register activation function
 register_activation_hook(__FILE__, 'vtl_url_rewrite_activate');
+
+// Register deactivation function
 register_deactivation_hook(__FILE__, 'vtl_url_rewrite_deactivate');
-add_action('init', 'vtl_url_rewrite_rules', 10, 0);
+
+// Add rewrite rules on init incase other plugin flushes rules
+add_action('init', 'vtl_url_rewrite_rules');
