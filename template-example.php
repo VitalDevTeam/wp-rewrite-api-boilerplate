2d6f30e9d8f4bd4470040c20fb2572971213a659
Adam Walter <adam@adamwalter.com>
2016-11-04 15:03:39 -0400

    Fix example template syntax

--- template-example.php
+++ template-example.php
@@ -1,6 +1,6 @@
 <?php if (have_posts()): while (have_posts()): the_post();
 
-    $widget_size = get_query_var('widget_size', 'default-value');
+    $widget_type = get_query_var('widget_type', 'default-value');
 
     echo $widget_type;
 
